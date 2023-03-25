94ba551f354a2f03def401f5061fef01527b8af6
Evangelos Apostolidis <evangelos.apostolidis@fieldcode.com>
2023-03-25 14:20:00 +0200

    first implementation

---
+++ src/main/java/com/example/xm/model/Bitcoin.java
@@ -0,0 +1,18 @@
+package com.example.xm.model;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Bitcoin {
+
+  private Timestamp timestamp;
+  private String symbol;
+  private BigDecimal price;
+}
---
+++ src/test/java/com/example/xm/service/BitcoinServiceTest.java
@@ -0,0 +1,40 @@
+package com.example.xm.service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.xm.model.Bitcoin;
+import com.example.xm.model.BitcoinMonthResults;
+import com.example.xm.model.NormalizeBitcoin;
+
+@SpringBootTest
+class BitcoinServiceTest {
+
+  @Autowired
+  BitcoinService bitcoinService;
+
+  @Test
+  void fetchBitCoins() {
+    List<Bitcoin> result = bitcoinService.fetchBitCoins("ETH", LocalDate.of(2000, 01, 01), LocalDate.of(2030,01,01));
+    Assertions.assertNotNull(result);
+  }
+
+  @Test
+  void calculateBitCoinsResults() {
+    List<Bitcoin> bitcoins = bitcoinService.fetchBitCoins("DOGE", LocalDate.of(2000, 01, 01), LocalDate.of(2030,01,01));
+    BitcoinMonthResults result = bitcoinService.calculateBitCoinsResults(bitcoins);
+    Assertions.assertNotNull(result);
+  }
+
+  @Test
+  void calculateNormalizeRange() {
+    List<NormalizeBitcoin> result = bitcoinService.calculateNormalizeRange(List.of("BTC", "DOGE", "ETH", "LTC", "XRP"), LocalDate.of(2000, 01, 01), LocalDate.of(2030,01,01));
+    Assertions.assertNotNull(result);
+
+  }
+}
